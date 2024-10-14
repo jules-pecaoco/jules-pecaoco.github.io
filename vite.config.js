d7e7a8bc0b1dccd807e75ccda551d471d73c31be
Jules Alfonz Pecaoco <114234327+jules-pecaoco@users.noreply.github.com>
2024-10-14 21:53:37 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jules-pecaoco.github.io/',
+  base: 'jules-pecaoco.github.io',
   plugins: [react()],
 })
